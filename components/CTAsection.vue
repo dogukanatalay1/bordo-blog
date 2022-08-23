5386cf695e10f6947d4e248558327f1244c35de9
Doğukan Atalay <dogukanatalay46@gmail.com>
2022-08-23 17:01:16 +0300

    cta second css tidy up

--- components/CTAsection.vue
+++ components/CTAsection.vue
@@ -2,10 +2,24 @@
   <section class="cta d-flex flex-column align-items-center">
     <div class="cta_container d-flex flex-column align-items-start order-0">
       <div
-        class="cta_container_content d-flex flex-column align-items-center p-0 order-0"
+        class="
+          cta_container_content
+          d-flex
+          flex-column
+          align-items-center
+          p-0
+          order-0
+        "
       >
         <div
-          class="cta_container_content_head-supp d-flex flex-column align-items-center p-0 order-0"
+          class="
+            cta_container_content_head-supp
+            d-flex
+            flex-column
+            align-items-center
+            p-0
+            order-0
+          "
         >
           <span class="cta_container_content_head-supp_heading order-0">Start your free trial</span>
           <p class="cta_container_content_head-supp_support-text order-1">
@@ -14,12 +28,39 @@
           </p>
         </div>
         <div
-          class="cta_container_content_actions d-flex flex-row align-items-start p-0 order-1"
+          class="
+            cta_container_content_actions
+            d-flex
+            flex-row
+            align-items-start
+            p-0
+            order-1
+          "
         >
-          <button class="cta_container_content_actions_btn btn-1 d-flex flex-row justify-content-center align-items-center order-0">
+          <button
+            class="
+              cta_container_content_actions_btn
+              btn-1
+              d-flex
+              flex-row
+              justify-content-center
+              align-items-center
+              order-0
+            "
+          >
             Learn more
           </button>
-          <button class="cta_container_content_actions_btn btn-2 d-flex flex-row justify-content-center align-items-center order-1">
+          <button
+            class="
+              cta_container_content_actions_btn
+              btn-2
+              d-flex
+              flex-row
+              justify-content-center
+              align-items-center
+              order-1
+            "
+          >
             Get started
           </button>
         </div>
@@ -33,5 +74,4 @@ export default {}
 </script>
 
 <style lang="scss" scoped>
-
 </style>
